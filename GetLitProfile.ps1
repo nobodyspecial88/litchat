ba7fde9ef4e03eeb2c9d48c00a9f6da275f4bf00
nobodyspecial88 <143667716+nobodyspecial88@users.noreply.github.com>
2023-08-30 17:09:36 -0700

    Add some scripts

---
+++ GetLitProfile.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+    [Parameter(Mandatory)]
+    [string]$username
+)
+
+$uri = "https://literotica.com/api/3/search/members?params=%7B%22q%22%3A%22$username%22%7D"
+
+$response = Invoke-WebRequest -Uri $uri
+
+$content = $response.Content | ConvertFrom-Json
+
+foreach ($item in $content.data)
+{
+    if ($item.username -eq $username)
+    {
+        $userid = $item.userid
+        $profileUri = "https://www.literotica.com/stories/memberpage.php?uid=$userid"
+        Start-Process $profileUri
+        break
+    }
+}
---
+++ GetLitToken.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+    [Parameter(Mandatory)]
+    [string]$username,
+    [Parameter(Mandatory)]
+    [string]$password
+)
+
+$timestamp = [DateTimeOffset]::Now.ToUnixTimeSeconds()
+
+Start-Sleep -Seconds 1
+
+$body_json = @{
+    login = $username
+    password = $password
+} | ConvertTo-Json
+
+$WebSession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+
+$response = Invoke-RestMethod -Uri https://auth.literotica.com/login -Method Post -Body $body_json -ContentType "application/json" -WebSession $WebSession
+
+$headers = ${
+    Referer = "https://chat.literotica.com/"
+}
+
+$uri = "https://auth.literotica.com/check?timestamp=$timestamp"
+
+Start-Sleep -Seconds 1
+
+$authResponse = Invoke-WebRequest -Uri $uri -Headers $headers -WebSession $WebSession
+
+$authResponse.Content
